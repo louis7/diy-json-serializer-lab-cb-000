a2540a29ed47d064f944c77b1b7cdc88aabe0ce7
louis baptiste <louisvis@ymail.com>
2020-05-10 04:53:35 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,14 +22,17 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
+
   def data
-     product = Product.find(params[:id])
-     render json: ProductSerializer.serialize(product)
-   end
-   
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
   end
-end
